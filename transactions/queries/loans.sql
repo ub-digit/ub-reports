cf443bd5fa107b9c10e18bd45d3f7bad5703397d
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2022-03-25 14:15:49 +0100

    Bug with start date for transaction report

--- transactions/queries/loans.sql
+++ transactions/queries/loans.sql
@@ -11,6 +11,6 @@ SELECT
     SUBSTRING(location, 1,2) AS agande_bibl, 
     COUNT(*) AS antal 
   FROM ub_statistics s 
-  WHERE DATE(s.datetime) BETWEEN '%%QUERY_YEAR%%-12-01' AND '%%QUERY_YEAR%%-12-31' 
+  WHERE DATE(s.datetime) BETWEEN '%%QUERY_YEAR%%-01-01' AND '%%QUERY_YEAR%%-12-31' 
     AND s.type IN ('issue', 'renew', 'onsite_checkout') 
   GROUP BY YEAR(s.datetime), MONTH(s.datetime), s.branch, s.categorycode, s.type, s.itemtype, SUBSTRING(location,1,2) 
